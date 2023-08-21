19803546c10f22cfe52ce0341b1a229c3a71fadc
Gill Shen <gillarete@gmail.com>
2023-08-21 23:24:17 +0800

    Re-style student anchor cards

--- frontend/src/lib/components/StudentAnchorCard.svelte
+++ frontend/src/lib/components/StudentAnchorCard.svelte
@@ -16,9 +16,7 @@
 		<header class={`contract-chip ${chipClasses[student.latest_contract_type]}`}>
 			{student.latest_contract_type}
 		</header>
-		<section class="flex-grow px-6 py-6 text-lg flex items-center">
-			{student.name}
-		</section>
+		<section>{student.name}</section>
 	</a>
 {:else}
 	<a href="../students/new/" class="student-anchor card">
@@ -31,16 +29,21 @@
 <style lang="postcss">
 	.student-anchor.card,
 	.student-anchor.card div {
-		@apply w-[14rem] min-h-[4rem] max-h-fit;
+		@apply w-[9rem] h-[9rem] max-w-[9rem];
 		@apply flex flex-col;
-		@apply card-hover rounded-sm;
+		@apply card-hover rounded-2xl;
+	}
+	.student-anchor section {
+		@apply text-lg;
+		@apply max-w-full overflow-hidden text-ellipsis;
+		@apply p-6;
 	}
 	.student-anchor:hover {
 		@apply text-primary-500;
 	}
 	header.contract-chip {
-		@apply px-2 py-1;
-		@apply rounded-t-sm text-surface-900;
-		@apply text-sm;
+		@apply px-6 py-2;
+		@apply rounded-t-2xl;
+		@apply text-sm text-surface-900;
 	}
 </style>
